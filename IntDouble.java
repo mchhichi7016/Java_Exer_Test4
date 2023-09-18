f297bce22de1853e892b8ad1ec264dd8cacd6520
MINAMINO <causewaybar711@gmail.com>
2023-09-18 10:22:53 +0900

    Create IntDouble.java

---
+++ IntDouble.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 1/10;
+        double b = 1/10.0;
+        //int c = (double) 1/10;
+        double d = 1/10;
+        double e = (double) (1/10);
+
+        System.out.println(a);
+        System.out.println(b);
+        //System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
+
